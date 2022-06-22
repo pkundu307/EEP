facd0ac367f3f7d42b075e2e9c72a801221f14fb
Prasanna Kundu <87819535+pkundu307@users.noreply.github.com>
2022-06-22 20:42:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,297 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+        navbar.classList.remove('active');
+        searchForm.classList.remove('active');
+        cartItem.classList.remove('active');
+    }
+    //clock
+setInterval(function() {
+    const clock = document.querySelector(".display");
+    let time = new Date();
+    let sec = time.getSeconds();
+    let min = time.getMinutes();
+    let hr = time.getHours();
+    let day = 'AM';
+    if (hr > 12) {
+        day = 'PM';
+        hr = hr - 12;
+    }
+    if (hr == 0) {
+        hr = 12;
+    }
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+    if (hr < 10) {
+        hr = '0' + hr;
+    }
+    clock.textContent = hr + ':' + min + ':' + sec + " " + day;
+});
+
+//alarm
+const currentTime = document.querySelector("h1"),
+    content = document.querySelector(".content"),
+    selectMenu = document.querySelectorAll("select"),
+    setAlarmBtn = document.querySelector("button");
+
+let alarmTime, isAlarmSet,
+    ringtone = new Audio("./files/ringtone.mp3");
+
+for (let i = 12; i > 0; i--) {
+    i = i < 10 ? `0${i}` : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+for (let i = 59; i >= 0; i--) {
+    i = i < 10 ? `0${i}` : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+for (let i = 2; i > 0; i--) {
+    let ampm = i == 1 ? "AM" : "PM";
+    let option = `<option value="${ampm}">${ampm}</option>`;
+    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
+}
+
+setInterval(() => {
+    let date = new Date(),
+        h = date.getHours(),
+        m = date.getMinutes(),
+        s = date.getSeconds(),
+        ampm = "AM";
+    if (h >= 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    h = h == 0 ? h = 12 : h;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+    if (alarmTime === `${h}:${m} ${ampm}`) {
+        ringtone.play();
+        ringtone.loop = true;
+    }
+});
+
+function setAlarm() {
+    if (isAlarmSet) {
+        alarmTime = "";
+        ringtone.pause();
+        content.classList.remove("disable");
+        setAlarmBtn.innerText = "Set Alarm";
+        return isAlarmSet = false;
+    }
+
+    let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
+    if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
+        return alert("Please, select a valid time to set Alarm!");
+    }
+    alarmTime = time;
+    isAlarmSet = true;
+    content.classList.add("disable");
+    setAlarmBtn.innerText = "Clear Alarm";
+}
+
+setAlarmBtn.addEventListener("click", setAlarm);
+//rps
+let [computer_score, user_score] = [0, 0];
+let result_ref = document.getElementById("result");
+let choices_object = {
+    'rock': {
+        'rock': 'draw',
+        'scissor': 'win',
+        'paper': 'lose'
+    },
+    'scissor': {
+        'rock': 'lose',
+        'scissor': 'draw',
+        'paper': 'win'
+    },
+    'paper': {
+        'rock': 'win',
+        'scissor': 'lose',
+        'paper': 'draw'
+    }
+
+}
+
+function checker(input) {
+    var choices = ["rock", "paper", "scissor"];
+    var num = Math.floor(Math.random() * 3);
+
+    document.getElementById("comp_choice").innerHTML =
+        ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
+
+    document.getElementById("user_choice").innerHTML =
+        ` You choose <span> ${input.toUpperCase()} </span>`;
+
+    let computer_choice = choices[num];
+
+    switch (choices_object[input][computer_choice]) {
+        case 'win':
+            result_ref.style.cssText = "background-color: #cefdce; color: #689f38";
+            result_ref.innerHTML = "YOU WIN";
+            user_score++;
+            break;
+        case 'lose':
+            result_ref.style.cssText = "background-color: #ffdde0; color: #d32f2f";
+            result_ref.innerHTML = "YOU LOSE";
+            computer_score++;
+            break;
+        default:
+            result_ref.style.cssText = "background-color: #e5e5e5; color: #808080";
+            result_ref.innerHTML = "DRAW";
+            break;
+    }
+
+    document.getElementById("computer_score").innerHTML = computer_score;
+    document.getElementById("user_score").innerHTML = user_score;
+}
+
+//weather
+let weather = {
+    apiKey: "4ded145d1df37c52a0aa6c624e24de6a    ",
+    fetchWeather: function(city) {
+        fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+            )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("No weather found.");
+                    throw new Error("No weather found.");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage =
+            "url('https://img.freepik.com/free-vector/damask-vintage-black-background-with-floral-elements-gothic-baroque-style-royal-texture-vector_619989-958.jpg?" + name + "')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function(event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather("Denver");
+
+//nasa
+const img = document.querySelector(".picture");
+const title = document.querySelector(".title");
+const explanation = document.querySelector(".explanation");
+const capturedate = document.querySelector(".capturedate");
+const copyright = document.querySelector(".copyright");
+
+let date, start = Date.parse('2021-01-01'),
+    end = new Date();
+end.setDate(end.getDate() - 1);
+const getDate = () => {
+    date = new Date(Math.floor(Math.random() * (end - start + 1) + start)).toISOString().split('T')[0];
+    return date
+};
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=${getDate()}&end_date=${date}`)
+    .then(response => response.json())
+    .then(data => {
+        img.src = data[0].url
+        title.innerText = data[0].title
+        explanation.innerText = data[0].explanation
+        capturedate.innerText = data[0].date
+        copyright.innerText = "Image Copyright by @ " + data[0].copyright
+    })
+
+function nextimg() {
+    window.location.reload();
+}
+//todo
+document.querySelector('#push').onclick = function() {
+    if (document.querySelector('#newtask input').value.length == 0) {
+        alert("Please Enter a Task")
+    } else {
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function() {
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for (var i = 0; i < tasks.length; i++) {
+            tasks[i].onclick = function() {
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
