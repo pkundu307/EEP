4fa469c53da29754f61b1fed7df7a3f7d518de58
Prasanna Kundu <87819535+pkundu307@users.noreply.github.com>
2022-06-23 11:34:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -264,10 +264,10 @@ function nextimg() {
 }
 //todo
 document.querySelector('#push').onclick = function() {
-    if (document.querySelector('#newtask input').value.length == 0) {
-        alert("Please Enter a Task")
-    } else {
-        document.querySelector('#tasks').innerHTML += `
+        if (document.querySelector('#newtask input').value.length == 0) {
+            alert("Please Enter a Task")
+        } else {
+            document.querySelector('#tasks').innerHTML += `
             <div class="task">
                 <span id="taskname">
                     ${document.querySelector('#newtask input').value}
@@ -278,20 +278,95 @@ document.querySelector('#push').onclick = function() {
             </div>
         `;
 
-        var current_tasks = document.querySelectorAll(".delete");
-        for (var i = 0; i < current_tasks.length; i++) {
-            current_tasks[i].onclick = function() {
-                this.parentNode.remove();
+            var current_tasks = document.querySelectorAll(".delete");
+            for (var i = 0; i < current_tasks.length; i++) {
+                current_tasks[i].onclick = function() {
+                    this.parentNode.remove();
+                }
             }
-        }
 
-        var tasks = document.querySelectorAll(".task");
-        for (var i = 0; i < tasks.length; i++) {
-            tasks[i].onclick = function() {
-                this.classList.toggle('completed');
+            var tasks = document.querySelectorAll(".task");
+            for (var i = 0; i < tasks.length; i++) {
+                tasks[i].onclick = function() {
+                    this.classList.toggle('completed');
+                }
             }
-        }
 
-        document.querySelector("#newtask input").value = "";
+            document.querySelector("#newtask input").value = "";
+        }
+    }
+    // movie
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+// initially get fav movies
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+}
+
+function showMovies(movies) {
+    // clear main
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
     }
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
